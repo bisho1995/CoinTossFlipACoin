1de35a8cad0018578330c9d73cfcdd0e1e5472b3
bisvarup <bisvarup.mukherjee@tracxn.com>
2019-10-27 00:12:21 +0530

    styles update

--- src/containers/home.js
+++ src/containers/home.js
@@ -1,25 +1,34 @@
 import React from 'react';
-import {TouchableOpacity, StyleSheet, Dimensions} from 'react-native';
+import {StyleSheet} from 'react-native';
 import {Container, Card, CardItem, Body, Row, Button} from 'native-base';
 import {Tail, View, Text} from '../components';
 import colors from '../styles/colors';
 import commonStyles from '../styles/common';
+import {dimensions} from '../utils/utils';
 
-const {width, height} = Dimensions.get('window');
+const {width, height} = dimensions;
 
 const dim = Math.min(width, height) - 30;
 
+const margin = {
+  marginTop: 10,
+  marginBottom: 10,
+};
+
 const styles = StyleSheet.create({
+  containerStyle: {
+    ...StyleSheet.absoluteFill,
+    ...commonStyles.flexColumn,
+    backgroundColor: colors.primary,
+    justifyContent: 'space-around',
+  },
   coinsWrapper: {
     justifyContent: 'center',
     width: '100%',
     height: dim,
     backgroundColor: colors.primary,
   },
-  margin: {
-    marginTop: 10,
-    marginBottom: 10,
-  },
+  margin,
   flipBtn: {
     width: '100%',
     maxWidth: 200,
@@ -27,25 +36,31 @@ const styles = StyleSheet.create({
     paddingTop: 30,
     paddingBottom: 30,
   },
+  footerSection: {
+    ...commonStyles.flexColumnCentered,
+    ...margin,
+    justifyContent: 'space-around',
+    paddingBottom: 10,
+    paddingTop: 10,
+  },
 });
 
 const Home = () => (
   <Container>
-    <View
-      style={{
-        ...StyleSheet.absoluteFill,
-        ...commonStyles.flexColumn,
-        backgroundColor: colors.primary,
-        justifyContent: 'space-around',
-      }}>
+    <View style={styles.containerStyle}>
       <Row style={{flex: 1}}>
         <View style={styles.margin}>
-          <Text textAlign="center" fontSize={40}>
-            Flip A Coin
-          </Text>
+          <Row style={{display: 'flex', justifyContent: 'space-between'}}>
+            <Text width="auto" fontSize={30}>
+              Flip A Coin
+            </Text>
+            <Text width="auto" style={{marginRight: 15, marginTop: 15}}>
+              Hamburger
+            </Text>
+          </Row>
         </View>
       </Row>
-      <Row style={{flex: 7}}>
+      <Row style={{flex: 6}}>
         <View style={{...commonStyles.flexRowCentered, ...styles.margin}}>
           <Card
             style={{
@@ -55,11 +70,7 @@ const Home = () => (
             <CardItem style={{backgroundColor: colors.primary}}>
               <Body style={{backgroundColor: colors.primary}}>
                 <Row style={styles.coinsWrapper}>
-                  <TouchableOpacity
-                    style={styles.coinsWrapper}
-                    onPress={() => console.log('pressed')}>
-                    <Tail dimension={dim - 40} />
-                  </TouchableOpacity>
+                  <Tail dimension={dim - 40} />
                 </Row>
               </Body>
             </CardItem>
@@ -74,12 +85,15 @@ const Home = () => (
         </View>
       </Row>
       <Row style={{flex: 2}}>
-        <View style={{...commonStyles.flexRowCentered, ...styles.margin}}>
-          <Button style={styles.flipBtn}>
-            <Text textAlign="center" fontSize={25}>
-              TOSS !
-            </Text>
-          </Button>
+        <View style={styles.footerSection}>
+          <View style={commonStyles.flexRowCentered}>
+            <Button style={styles.flipBtn}>
+              <Text textAlign="center" fontSize={25}>
+                TOSS !
+              </Text>
+            </Button>
+          </View>
+          <Text textAlign="center">Themes</Text>
         </View>
       </Row>
     </View>
