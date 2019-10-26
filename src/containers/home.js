3e1ae00496ac8907cbe585cd7ea23047719efe50
bisvarup <bisvarup.mukherjee@tracxn.com>
2019-10-26 18:24:33 +0530

    basic display of two images

--- src/containers/home.js
+++ src/containers/home.js
@@ -1,6 +1,65 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Image, TouchableOpacity} from 'react-native';
+import {
+  Container,
+  Content,
+  Card,
+  CardItem,
+  View,
+  Body,
+  Text,
+  Row,
+} from 'native-base';
+import styled from 'styled-components';
+import LionImageSrc from '../assets/images/lion.jpg';
+import PeacockImageSrc from '../assets/images/peacock.jpg';
 
-const Home = () => <Text>Home working</Text>;
+const StyledImage = styled.Image`
+  flex: 1;
+  width: 100%;
+`;
 
+const ImageWrapper = styled.View`
+  width: 300;
+  height: 300;
+  position: absolute;
+  border-radius: 150;
+  top: 0;
+`;
+
+const Home = () => (
+  <Container>
+    <Content>
+      <Card>
+        <CardItem>
+          <Body>
+            <Row
+              style={{
+                justifyContent: 'center',
+                width: '100%',
+                height: 300,
+              }}>
+              <TouchableOpacity
+                style={{
+                  position: 'relative',
+                  flex: 1,
+                  display: 'flex',
+                  flexDirection: 'row',
+                  justifyContent: 'center',
+                }}
+                onPress={() => console.log('pressed')}>
+                <ImageWrapper>
+                  <StyledImage source={LionImageSrc} />
+                </ImageWrapper>
+                <ImageWrapper>
+                  <StyledImage source={PeacockImageSrc} />
+                </ImageWrapper>
+              </TouchableOpacity>
+            </Row>
+          </Body>
+        </CardItem>
+      </Card>
+    </Content>
+  </Container>
+);
 export default Home;
