a4da5607d1f7af6d0f7aa3ed5f99937d896e472b
bisvarup <bisvarup.mukherjee@tracxn.com>
2019-11-16 10:52:48 +0530

    button size and memo

--- src/components/Settings/ResetApp.js
+++ src/components/Settings/ResetApp.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {memo} from 'react';
 import commonStyles from '../../styles/common';
 import {connect} from 'react-redux';
 import Heading from '../Heading';
@@ -7,53 +7,56 @@ import View from '../View';
 import {Row, Button} from 'native-base';
 import {StyleSheet} from 'react-native';
 
-const ResetApp = ({colors, resetSettings, resetApp}) => {
-  const styles = StyleSheet.create({
-    row: {
-      ...commonStyles.marginTiny,
-      flex: 1,
-    },
-    text: {fontSize: 12},
-  });
-  return (
-    <View
-      style={{
-        backgroundColor: colors.error,
-        ...commonStyles.padding,
-        ...commonStyles.margin,
-      }}>
-      <Heading
-        title="Reset"
-        icon={{name: 'exclamation-triangle', color: '#D50000'}}
-      />
-      <View>
-        <Row style={styles.row}>
-          <View>
-            <Text style={styles.text}>Reset Settings</Text>
-          </View>
-          <Button
-            onPress={resetSettings}
-            small
-            style={{maxWidth: 100, backgroundColor: colors.primaryDark}}>
-            <Text textAlign="center">Reset Settings</Text>
-          </Button>
-        </Row>
-        <Row style={styles.row}>
-          <View>
-            <Text style={styles.text}>Reset App</Text>
-          </View>
+const styles = StyleSheet.create({
+  row: {
+    ...commonStyles.marginTiny,
+    flex: 1,
+  },
+  text: {fontSize: 12, marginTop: 5},
+  button: {
+    maxWidth: 120,
+    height: 45,
+  },
+});
 
-          <Button
-            onPress={resetApp}
-            small
-            style={{maxWidth: 100, backgroundColor: colors.primaryDark}}>
-            <Text textAlign="center">Reset App</Text>
-          </Button>
-        </Row>
-      </View>
+const ResetApp = ({colors, resetSettings, resetApp}) => (
+  <View
+    style={{
+      backgroundColor: colors.error,
+      ...commonStyles.padding,
+      ...commonStyles.margin,
+    }}>
+    <Heading
+      title="Reset"
+      icon={{name: 'exclamation-triangle', color: '#D50000'}}
+    />
+    <View>
+      <Row style={styles.row}>
+        <View>
+          <Text style={styles.text}>Reset Settings</Text>
+        </View>
+        <Button
+          onPress={resetSettings}
+          small
+          style={{...styles.button, backgroundColor: colors.primaryDark}}>
+          <Text textAlign="center">Reset Settings</Text>
+        </Button>
+      </Row>
+      <Row style={styles.row}>
+        <View>
+          <Text style={styles.text}>Reset App</Text>
+        </View>
+
+        <Button
+          onPress={resetApp}
+          small
+          style={{...styles.button, backgroundColor: colors.primaryDark}}>
+          <Text textAlign="center">Reset App</Text>
+        </Button>
+      </Row>
     </View>
-  );
-};
+  </View>
+);
 
 const mapStateToProps = ({
   ThemeReducer: {theme},
@@ -77,4 +80,4 @@ const mapDispatchToProps = dispatch => {
 export default connect(
   mapStateToProps,
   mapDispatchToProps,
-)(ResetApp);
+)(memo(ResetApp));
